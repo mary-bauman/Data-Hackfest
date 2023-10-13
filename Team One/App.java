48364a6f194a3926f1ebea0668c3cdb552a223aa
marybauman <marybauman503@gmail.com>
2023-10-13 14:57:14 -0400

    message

---
+++ Team One/App.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class App extends JFrame {
+    // Declare Swing components
+    public JButton myButton;
+    public JLabel myLabel;
+    public JTextField myTextField;
+
+    public static void main(String[] args) {
+        // Create an instance of App
+        SwingUtilities.invokeLater(() -> new App());
+    }
+
+    public App() {
+        // Set the title for the JFrame
+        super("My Java Swing Application");
+
+        // Initialize Swing components
+        myButton = new JButton("Click Me");
+        myLabel = new JLabel("Hello, World!");
+        myTextField = new JTextField(20);
+
+        // Set the layout manager for the JFrame (e.g., BorderLayout)
+        setLayout(new BorderLayout());
+
+        // Add components to the JFrame
+        add(myButton, BorderLayout.NORTH);
+        add(myLabel, BorderLayout.CENTER);
+        add(myTextField, BorderLayout.SOUTH);
+
+        // Set default close operation and window size
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 200);
+
+        // Add event listeners (e.g., for button clicks)
+        myButton.addActionListener(e -> {
+            String text = myTextField.getText();
+            myLabel.setText("Hello, " + text + "!");
+        });
+
+        // Make the JFrame visible
+        setVisible(true);
+    }
+
+
+}
