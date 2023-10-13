541e7a22c35fc6e3baf24406f31609ba06f06f4a
ferntherobot <19steind@gmail.com>
2023-10-13 18:53:43 -0400

    see MadLib.java for doing multiple mad libs

--- Team One/buttons/ButtonA.java
+++ Team One/buttons/ButtonA.java
@@ -2,6 +2,7 @@ package buttons;
 
 import javax.swing.*;
 
+import pages.MadLib;
 import pages.madLib1;
 
 
@@ -15,23 +16,8 @@ public class ButtonA {
     public JButton generateButtonA(JTextField tf, JLabel label){
         myButton = new JButton("Decisions");
         myButton.addActionListener(e -> {
-            switch(count) {
-                case 0:
-                    madLib1.color = tf.getText();
-                    tf.setText("");
-                    madLib1.myLabel.setText("Enter a name:");
-                    break;
-                case 1:
-                    madLib1.name = tf.getText();
-                    tf.setText("");
-                    madLib1.myLabel.setText("Enter a food:");
-                    break;
-                default:
-                   madLib1.food = tf.getText();
-                    tf.setText("");
-                    madLib1.myLabel.setText("You are a " + madLib1.color + " duckling named " + madLib1.name + " who wants " + madLib1.food +".");
-            }
-            count++;
+            String[] prompts = {"color", "name", "food"};
+            MadLib.generateMadLib("Team One/pages/madLib1.txt", prompts, false);
         });
         
         return myButton;
--- Team One/src/Main.java
+++ Team One/src/Main.java
@@ -2,14 +2,14 @@ package src;
 import javax.swing.*;
 
 import buttons.ButtonA;
-import pages.madLib1;
+import pages.MadLib;
 import java.awt.*;
 
 public class Main extends JFrame {
     
     public static void main(String[] args) {
         // Create an instance of Main
-        SwingUtilities.invokeLater(() -> new madLib1());
+        SwingUtilities.invokeLater(() -> new MadLib());
     }
 
 
