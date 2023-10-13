11b1adecd044be58b25d2877af3ad39ed02d25e1
marybauman <marybauman503@gmail.com>
2023-10-13 16:35:58 -0400

    check it out

---
+++ Team One/src/ButtonB.java
@@ -0,0 +1,8 @@
+package src;
+
+public class ButtonB {
+    public ButtonB(){
+        
+    }
+}
+
--- Team One/src/Main.java
+++ Team One/src/Main.java
@@ -1,7 +1,8 @@
+package src;
 import javax.swing.*;
 import java.awt.*;
 
-public class App extends JFrame {
+public class Main extends JFrame {
     // Declare Swing components
     public JButton myButton;
     public JLabel myLabel;
@@ -12,40 +13,35 @@ public class App extends JFrame {
     String name = "Duck";
     String food = "Bread";
 
+    
     public static void main(String[] args) {
-        // Create an instance of App
-        SwingUtilities.invokeLater(() -> new App());
+        // Create an instance of Main
+        SwingUtilities.invokeLater(() -> new Main());
     }
 
-    public App() {
+    public Main() {
         // Set the title for the JFrame
-        super("My Java Swing Application");
-
+        super("Main");
 
         // Initialize Swing components
+        myLabel = new JLabel("Hello, World!");
         myTextField = new JTextField(20);
-
-        myLabel = new JLabel("You are a " + color + " duckling named " + name + ".");
-        myButton = new JButton("Decisions");
-
+        //ButtonA a = new ButtonA();
+        ButtonB b = new ButtonB();
+        //JButton buttonA = a.generateButtonA(myTextField,myLabel);
 
         // Set the layout manager for the JFrame (e.g., BorderLayout)
         setLayout(new BorderLayout());
 
         // Add components to the JFrame
-        add(myButton, BorderLayout.NORTH);
+        //add(myButton, BorderLayout.NORTH);
         add(myLabel, BorderLayout.CENTER);
         add(myTextField, BorderLayout.SOUTH);
+        //add(buttonA, BorderLayout.EAST);
 
         // Set default close operation and window size
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 200);
-
-        // Add event listeners (e.g., for button clicks)
-        myButton.addActionListener(e -> {
-            String text = myTextField.getText();
-            myLabel.setText("Hello, " + text + "!");
-        });
 
         // Make the JFrame visible
         setVisible(true);
